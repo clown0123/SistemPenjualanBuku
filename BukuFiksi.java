53c1e633eea3453aa6c2dbc03b00b762fd8091b7
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-07-14 21:09:28 +0700

    Create BukuFiksi.java

---
+++ BukuFiksi.java
@@ -0,0 +1,25 @@
+public abstract class BukuFiksi extends Buku {
+    public BukuFiksi(String judul, int hargaBeli, int hargaJual, int jumlahStok) {
+        super(judul, hargaBeli, hargaJual, jumlahStok);
+    }
+
+    @Override
+    public void tambahStok(int jumlah) {
+        jumlahStok += jumlah;
+    }
+
+    public void kurangiStok(int jumlah) {
+        jumlahStok -= jumlah;
+        if (jumlahStok <= 0) {
+            System.out.println("Stok " + judul + " habis.");
+        }
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Harga Beli: " + hargaBeli);
+        System.out.println("Harga Jual: " + hargaJual);
+        System.out.println("Jumlah Stok: " + jumlahStok);
+    }
+}
