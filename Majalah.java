2b0627a03fba2571acca3a63db37f4d213e19bf1
clown0123 <115709696+clown0123@users.noreply.github.com>
2023-07-18 00:07:23 +0700

    Update Majalah.java

--- Majalah.java
+++ Majalah.java
@@ -1,29 +1,16 @@
-public abstract class Majalah extends Buku {
+class Majalah extends Buku {
     private int nomorEdisi;
-
-    public Majalah(String judul, int hargaBeli, int hargaJual, int jumlahStok, int nomorEdisi) {
-        super(judul, hargaBeli, hargaJual, jumlahStok);
+ 
+    public Majalah(String judul, double hargaBeli, double hargaJual, int stok, int nomorEdisi) {
+        super(judul, hargaBeli, hargaJual, stok);
         this.nomorEdisi = nomorEdisi;
     }
-
-    @Override
-    public void tambahStok(int jumlah) {
-        jumlahStok += jumlah;
-    }
-
-    public void kurangiStok(int jumlah) {
-        jumlahStok -= jumlah;
-        if (jumlahStok <= 0) {
-            System.out.println("Stok " + judul + " habis.");
-        }
-    }
-
+ 
     @Override
     public void tampilkanInfo() {
+        System.out.println("Jenis: Majalah");
         System.out.println("Judul: " + judul);
-        System.out.println("Harga Beli: " + hargaBeli);
-        System.out.println("Harga Jual: " + hargaJual);
-        System.out.println("Jumlah Stok: " + jumlahStok);
         System.out.println("Nomor Edisi: " + nomorEdisi);
+        System.out.println("Stok: " + stok);
     }
 }
